44b8d88e8a5632853cf3bbe966b012928e24d9d7
Austin <funcychaos@gmail.com>
2025-07-15 10:41:09 -0700

    feat(models): models are functioning at mvp

--- porchfestcore/admin.py
+++ porchfestcore/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Performer, Porch, Request, Performance
 
 # Register your models here.
+
+admin.site.register(Performer)
+admin.site.register(Porch)
+admin.site.register(Request)
+admin.site.register(Performance)
