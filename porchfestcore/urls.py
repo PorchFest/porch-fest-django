a8ed10fe56eddbde22a7a88a677c7e4b1ed1c62f
Austin Reilly <funcychaos@gmail.com>
2025-08-30 11:06:08 -0700

    feat(call-porches): make form, adjust model, verify filtering functionality

--- porchfestcore/urls.py
+++ porchfestcore/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	path('', views.IndexView.as_view(), name='index'),
 	path('map/', views.map_page, name='map'),
 	path('pages/', views.pages, name='pages'),
+	path('call-porches/', views.call_porches, name='call-porches'),
 ]
--- porchfestcore/views.py
+++ porchfestcore/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts 		import render
 from django.views.generic 	import TemplateView
-
-# Create your views here.
+from .forms					import PorchForm
 
 class IndexView(TemplateView):
 	template_name = 'porchfestcore/index.html'
@@ -11,3 +10,19 @@ def map_page(request):
 
 def pages(request):
 	return render(request, 'porchfestcore/pages.html')
+
+def call_porches(request):
+    if request.method == 'POST':
+        form 				= PorchForm(request.POST)
+        if form.is_valid():
+            porch 			= form.save(commit=False)
+            porch.approved 	= False
+            porch.save()
+            messages.success(request, 'Thank you! Your porch has been submitted for review.')
+            return redirect('porch_submitted')
+    else:
+        form 				= PorchForm()
+    return render(request, 'porchfestcore/call-porches.html', {'form': form})
+
+def porch_submitted(request):
+    return render(request, 'porchfestcore/porch_submitted.html')
