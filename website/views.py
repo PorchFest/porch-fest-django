e6b2db023ec88e9ad4abda2ecaf5b607c4b1e4e4
Austin Reilly <funcychaos@gmail.com>
2025-08-31 21:59:32 -0700

    feat(front-page): add sponsors

--- website/views.py
+++ website/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts 		import render
 from django.views.generic 	import TemplateView
+from .models				import Sponsor
 
 class IndexView(TemplateView):
-    template_name = 'website/index.html'
+    template_name 	= 'website/index.html'
 
 def pages(request):
-    return render(request, 'website/pages.html')
+    sponsors 		= Sponsor.objects.filter(is_active=True).order_by("level", "name")
+    return render(request, 'website/front-page/pages.html', {"sponsors": sponsors})
 
 def about(request):
     return render(request, 'website/about.html')
