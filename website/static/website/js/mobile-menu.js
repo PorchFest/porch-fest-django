760981e41bec175e3d926c29c68c1dc72fdde181
Austin Reilly <funcychaos@gmail.com>
2025-09-03 14:25:43 -0700

    feat(header): add mobile header html, js, and css.
    needs lots more work, this should help all of my sites
    sticky is cool until it isn't eh

---
+++ website/static/website/js/mobile-menu.js
@@ -0,0 +1,39 @@
+window.addEventListener('resize', setVhUnit)
+
+function setVhUnit(){
+  const vh 				= window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+}
+setVhUnit()
+
+document.addEventListener('DOMContentLoaded', ()=>{
+	const mobileMenu 	= document.getElementById('mobile_menu')
+	const page 			= document.getElementById('page')
+	let showMenu 		= false
+	document.getElementById('mobile_toggle').addEventListener('click', ()=>{
+		displayToggle(mobileMenu)
+	})
+	mobileMenu.addEventListener('click', ()=>{
+		displayToggle(mobileMenu)
+	})
+	mobileMenu.addEventListener('transitionend', ()=>{
+		removeTransition(mobileMenu)
+	})
+	function displayToggle(el){
+		if(!showMenu){
+			// document.body.style.overflow = 'hidden';
+			el.classList.add('transition')
+			el.clientWidth
+			el.classList.remove('hidden')
+			showMenu 	= true
+		}else{
+			document.body.style.overflow = '';
+			el.classList.add('transition')
+			el.classList.add('hidden')
+			showMenu 	= false
+		}
+	}
+	function removeTransition(el){
+		el.classList.remove('transition')
+	}
+})
