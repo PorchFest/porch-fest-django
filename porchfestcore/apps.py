b63ba223ecbb363a3f708ecd91be873bab192909
Austin Reilly <funcychaos@gmail.com>
2025-09-04 12:27:42 -0700

    feat(admin): app verbose names for peeps

--- porchfestcore/apps.py
+++ porchfestcore/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PorchfestcoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'porchfestcore'
+    verbose_name = 'Porch Fest!'
