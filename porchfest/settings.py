236b49a1e86aa1270ed7d3b197a0b9dabe2712a1
Austin Reilly <funcychaos@gmail.com>
2025-09-04 19:00:30 -0700

    chore(settings): fix settings for production

--- porchfest/settings.py
+++ porchfest/settings.py
@@ -134,22 +134,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
+LANGUAGE_CODE   = 'en-us'
+TIME_ZONE       = 'America/Los_Angeles'
+USE_I18N        = True
+USE_TZ          = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-# 	BASE_DIR / '/static/',
-# ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
