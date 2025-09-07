9c0bcadd277357c7ffe5b237edac42222f44c30b
Austin Reilly <funcychaos@gmail.com>
2025-09-07 10:05:24 -0700

    feat(favicon): add favicon

--- porchfest/settings.py
+++ porchfest/settings.py
@@ -145,6 +145,7 @@ USE_TZ          = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # new
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
