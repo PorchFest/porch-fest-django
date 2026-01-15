38faa7a9b28dff6ee649542348fe6270938631d7
Austin Reilly <funcychaos@gmail.com>
2026-01-14 21:39:54 -0800

    feat(admin): add csv export

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,29 @@
 from django.contrib 	import admin
 from .models			import Sponsor, PorchInterest
+from django.http		import HttpResponse
+import csv
+
+@admin.register(PorchInterest)
+class PorchInterestAdmin(admin.ModelAdmin):
+    actions         = ['export_as_csv']
+    list_display    = ('id', 'owner_name', 'owner_email', 'street_address',)
+
+    def export_as_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="porch_interests.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(['Name', 'Email', 'Address'])
+
+        for obj in queryset:
+            writer.writerow([
+                obj.id,
+                obj.owner_name,
+                obj.owner_email,
+                obj.street_address,
+            ])
+
+        return response
+    export_as_csv.short_description = "Export selected Porch Interests to CSV"
 
 admin.site.register(Sponsor)
-admin.site.register(PorchInterest)
