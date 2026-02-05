06706f6754fcc8cf282b670b3fbd75dc2f83d60e
Austin Reilly <funcychaos@gmail.com>
2026-02-05 12:23:07 -0800

    update(admin): Title admin for tower porchfest :P

--- porchfest/urls.py
+++ porchfest/urls.py
@@ -18,6 +18,9 @@ from django.contrib 			import admin
 from django.urls 				import path, include
 from django.conf 				import settings
 from django.conf.urls.static	import static
+admin.site.site_header 			= 'Tower Porchfest Admin'
+admin.site.index_title			= 'Admin'
+admin.site.site_title			= 'Porchfest'
 
 urlpatterns = [
 	path('', include('website.urls')),
