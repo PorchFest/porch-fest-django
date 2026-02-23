867b4d862c5ffff18bf98c6b449af6b2743709c7
Austin Reilly <funcychaos@gmail.com>
2026-02-23 11:00:45 -0800

    chore(countdown): simplify javascript

--- website/static/website/js/countdown.js
+++ website/static/website/js/countdown.js
@@ -1,46 +1,17 @@
-// Set the date we're counting down to
-
-
-// var countDownDate = new Date(wpVars.acfDate).getTime()
-var countDownDate = new Date(2026, 3, 25, 10, 0, 0).getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="counter"
-  // document.getElementById("counter").innerHTML = days + " " + hours + " "
-  // + minutes + " " + seconds + " ";
-  document.getElementById("days").innerHTML = days;
-  document.getElementById("hours").innerHTML = hours;
-  document.getElementById("minutes").innerHTML = minutes;
-  document.getElementById("seconds").innerHTML = seconds;
-  // document.getElementById("days-desk").innerHTML = days;
-  // document.getElementById("hours-desk").innerHTML = hours;
-  // document.getElementById("minutes-desk").innerHTML = minutes;
-  // document.getElementById("seconds-desk").innerHTML = seconds;
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("counter").innerHTML = "It was a blast!";
+const countDownDate = new Date(2026, 3, 25, 10, 0, 0).getTime()
+let x = setInterval(()=>{
+  const now         = new Date().getTime()
+  const distance    = countDownDate - now
+  const days        = Math.floor(distance / (1000 * 60 * 60 * 24))
+  const hours       = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  const minutes     = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+  const seconds     = Math.floor((distance % (1000 * 60)) / 1000)
+  document.getElementById("days").innerHTML       = days
+  document.getElementById("hours").innerHTML      = hours
+  document.getElementById("minutes").innerHTML    = minutes
+  document.getElementById("seconds").innerHTML    = seconds
+  if(distance < 0){
+    clearInterval(x)
+    document.getElementById("counter").innerHTML  = "Porchfest!"
   }
-}, 1000);
-
-// jQuery( document ).ready( function( $ ) {
-
-//   // console.log( countdownData.festivalDate );
-
-// });
-// const img = document.getElementById('sponsor-logo');
+}, 1000)
