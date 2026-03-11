608a961596090b1d26dc26d0f10181498c0e0873
Austin Reilly <funcychaos@gmail.com>
2026-03-10 23:41:09 -0700

    feat(api): use django-filters and list view for porch filtering

--- porchfestcore/api/filters.py
+++ porchfestcore/api/filters.py
@@ -2,8 +2,17 @@ import django_filters
 from porchfestcore.models import Porch
 
 class PorchFilter(django_filters.FilterSet):
-    genre = django_filters.CharFilter(field_name='performances__performer__genre', lookup_expr='iexact')
+    genre       = django_filters.CharFilter(
+        field_name='performances__performer__genre',
+        lookup_expr='iexact'
+    )
+    after       = django_filters.TimeFilter(
+        field_name='performances__start_time',
+        lookup_expr='gte'
+    )
+    vendor      = django_filters.BooleanFilter(field_name='vendor')
+    sponsored   = django_filters.BooleanFilter(field_name='sponsored')
 
     class Meta:
-        model = Porch
-        fields = ['genre']
+        model   = Porch
+        fields  = ['genre', 'after', 'vendor', 'sponsored']
