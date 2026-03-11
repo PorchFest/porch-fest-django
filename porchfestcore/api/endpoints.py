db5038c7ef0cc63834c81f977cceb7d5c7ff9fe4
Austin Reilly <funcychaos@gmail.com>
2026-03-11 16:57:52 -0700

    chore(api): naming again

--- porchfestcore/api/endpoints.py
+++ porchfestcore/api/endpoints.py
@@ -3,10 +3,10 @@ from rest_framework.views       import APIView
 from rest_framework.response    import Response
 from django.http                import JsonResponse
 from .serializers               import PorchesSerializer
-from .filters                   import PorchFilter
+from .filters                   import PorchMapFilter
 from porchfestcore.models       import Porch
 
 class PorchMap(ListAPIView):
     queryset            = Porch.objects.filter(approved=True).distinct()
     serializer_class    = PorchesSerializer
-    filterset_class     = PorchFilter
+    filterset_class     = PorchMapFilter
--- porchfestcore/api/filters.py
+++ porchfestcore/api/filters.py
@@ -1,7 +1,7 @@
 import django_filters
 from porchfestcore.models import Porch
 
-class PorchFilter(django_filters.FilterSet):
+class PorchMapFilter(django_filters.FilterSet):
     genre           = django_filters.CharFilter(
         field_name  ='performances__performer__genre',
         lookup_expr ='iexact'
