c686e872792c378d0c6b47c4b76abe7146cf6ce0
Austin Reilly <funcychaos@gmail.com>
2026-03-12 00:13:19 -0700

    feat(api): simplify api some more

--- porchfestcore/api/endpoints.py
+++ porchfestcore/api/endpoints.py
@@ -2,11 +2,11 @@ from rest_framework.generics    import ListAPIView
 from rest_framework.views       import APIView
 from rest_framework.response    import Response
 from django.http                import JsonResponse
-from .serializers               import PorchesSerializer
+from .serializers               import PorchMapSerializer
 from .filters                   import PorchMapFilter
 from porchfestcore.models       import Porch
 
 class PorchMap(ListAPIView):
     queryset            = Porch.objects.filter(approved=True).distinct()
-    serializer_class    = PorchesSerializer
     filterset_class     = PorchMapFilter
+    serializer_class    = PorchMapSerializer
