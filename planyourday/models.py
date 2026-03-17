90a66ec6240abe348518be29c261dfade387dccf
Austin Reilly <funcychaos@gmail.com>
2026-03-16 17:19:41 -0700

    feat(planyourday): add itinerary model

--- planyourday/models.py
+++ planyourday/models.py
@@ -1,3 +1,16 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Itinerary(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    performances = models.ManyToManyField('porchfestcore.Performance', related_name='itineraries', blank=True)
+
+    def ordered_performances(self):
+        return self.performances.select_related(
+            'performer',
+            'porch'
+        ).order_by('start_time')
+
+    def __str__(self):
+        return f"{self.id}"
