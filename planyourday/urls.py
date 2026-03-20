2b855cd51efd7a2a788df51f845d96a799ab2e36
Austin Reilly <funcychaos@gmail.com>
2026-03-20 11:01:50 -0700

    feat(planyourday): add remove view and test working theory

--- planyourday/urls.py
+++ planyourday/urls.py
@@ -4,5 +4,6 @@ from . 				import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('performances/', views.PerformancesListView.as_view(), name='performances'),
-    path('add-performance/<str:itinerary_id>', views.add_performance, name='add_performance')
+    path('add-performance/<str:itinerary_id>', views.add_performance, name='add_performance'),
+    path('remove-performance/<str:itinerary_id>', views.remove_performance, name='remove_performance'),
 ]
--- planyourday/views.py
+++ planyourday/views.py
@@ -22,6 +22,25 @@ def index(request):
 
 # I added some more stuff so that we can eventually update the itinerary when a performance gets added (or removed :P) I'm commiting the html files so that the idea makes sense
 
+# More updates I added a remove performance path. Now the only thing I think we need to api is creating an itinerary if we don't have one. Something like check to see if localstorage exists, if not create itinerary which will return the itinerary id for future requests. Example how it works from plan your day right now:
+
+# <div
+#     style="width: 100px;height: 100px;background-color: blue;color: white;"
+#     hx-post="http://localhost:8300/plan-your-day/remove-performance/740897a5-0765-4056-8336-fda6842ffdb2"
+#     hx-trigger="click"
+#     hx-target="#main_target"
+#     hx-vals='{"performance_id": "94747a76-72d5-4ac9-8f50-99a0e624f1bb"}'
+# >
+#     click here
+# </div>
+# <div id="main_target">
+
+# </div>
+
+# <div id="itinerary_sidebar">
+
+# </div>
+
 class PerformancesListView(ListView):
     template_name = 'planyourday/performance-list.html'
     context_object_name = 'performances'
@@ -37,16 +56,25 @@ class PerformancesListView(ListView):
 
 @csrf_exempt
 def add_performance(request, itinerary_id):
-    itinerary = get_object_or_404(Itinerary, id=itinerary_id)
-    performance_id = request.POST.get('performance_id')
-    performance = get_object_or_404(Performance, id=performance_id)
-
+    itinerary       = get_object_or_404(Itinerary, id=itinerary_id)
+    performance_id  = request.POST.get('performance_id')
+    performance     = get_object_or_404(Performance, id=performance_id)
     if not itinerary.performances.filter(id=performance.id).exists():
         itinerary.performances.add(performance)
-
     context = {
         "performance": performance,
         "itinerary": itinerary.ordered_performances(),
     }
+    return render(request, "planyourday/performance-detail-update.html", context)
 
-    return render(request, "planyourday/performance-detail.html", context)
+@csrf_exempt
+def remove_performance(request, itinerary_id):
+    itinerary       = get_object_or_404(Itinerary, id=itinerary_id)
+    performance_id  = request.POST.get('performance_id')
+    performance     = get_object_or_404(Performance, id=performance_id)
+    itinerary.performances.remove(performance)
+    context = {
+        "performance": performance,
+        "itinerary": itinerary.ordered_performances(),
+    }
+    return render(request, "planyourday/performance-detail-update.html", context)
