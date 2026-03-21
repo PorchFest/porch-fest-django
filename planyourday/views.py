a1a7d284d884439300a6561dbc82f4878905bd3f
Juan Lucero <luceroweb@gmail.com>
2026-03-20 20:40:52 -0700

    convert search and performances list to django/htmx calls

--- planyourday/views.py
+++ planyourday/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from porchfestcore.models   import Performance
+from porchfestcore.models   import Performance, Performer
 from planyourday.api.filters           import PerformanceFilter
 
 def index(request):
     performances = Performance.objects.filter(porch__approved=True).distinct()
-    return render(request, 'planyourday/index.html', {'performances': performances})
+    genres = Performer.Genre.choices
+    performance_count = len(performances)
+    return render(request, 'planyourday/index.html', {'performances': performances, 'genres': genres, 'performance_count':performance_count})
 
 class PerformancesListView(ListView):
     template_name = 'planyourday/performances.html'
@@ -19,3 +21,12 @@ class PerformancesListView(ListView):
             return self.filterset.qs
         
         return qs
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        qs = self.object_list
+
+        context['performance_count'] = len(qs)
+        context['filterset'] = self.filterset
+
+        return context
