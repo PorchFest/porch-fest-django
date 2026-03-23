098c73ded71f60d92ce3eb3ede3c4e3ce386d9e6
Austin Reilly <funcychaos@gmail.com>
2026-03-22 18:38:43 -0700

    chore(planyourday): remove comments

--- planyourday/views.py
+++ planyourday/views.py
@@ -9,37 +9,6 @@ def plan_your_day(request):
     itinerary       = get_or_create_itinerary(request)
     return render(request, 'planyourday/index.html', {'performances': performances, 'itinerary': itinerary.ordered_performances()})
 
-# Notes for rebuilt performances view with add to itinerary logic
-# performances.html is performance-list.html and looks like this now:
-# {% for performance in performances %}
-#     {% include "planyourday/performance-detail.html" %}
-# {% endfor %}
-
-# Add performance handles the itinerary logic and returns the performance html with the updated checkmark (or it should :)). This is the POST url for it:
-# /plan-your-day/add-performance
-# With a body/form data {performance_id: 94747a76-72d5-4ac9-8f50-99a0e624f1bb}
-
-# I added some more stuff so that we can eventually update the itinerary when a performance gets added (or removed :P) I'm commiting the html files so that the idea makes sense
-# Actually I think I pretty much got it there? heckin...
-
-# More updates I added a remove performance path. Now the only thing I think we need to api is creating an itinerary if we don't have one. Something like check to see if localstorage exists, if not create itinerary which will return the itinerary id for future requests. Example how it works from plan your day right now:
-# UPDATE AGAAIN: I used sessions in the view to keep track of the itinerary per session. Everything should just work off of performance_id now :O
-
-# I also included the itinerary in the default page load too so it should be ready to wire up I think? I'm sure we're missing stuff and the filtering is of course not ready but should be good otherwise
-
-# Here is the htmx example:
-
-# <div
-#     style="width: 100px;height: 100px;background-color: blue;color: white;"
-#     hx-post="http://localhost:8300/plan-your-day/add-performance"
-#     hx-trigger="click"
-#     hx-vals='{"performance_id": "83460f22-298a-47d0-8d94-1503060aa7bf"}'
-#     hx-headers='{"X-CSRFToken": "{{ csrf_token }}"}'
-# >
-#     click here
-# </div>
-# <div id="itinerary_sidebar"></div>
-
 class PerformancesListView(ListView):
     template_name       = 'planyourday/performance-list.html'
     context_object_name = 'performances'
