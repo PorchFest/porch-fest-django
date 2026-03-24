6c1711295f2de306b4ac686d4858bc4e43b9243e
Austin Reilly <funcychaos@gmail.com>
2026-03-24 12:21:31 -0700

    update(planyourday): change url, add -now

--- porchfest/urls.py
+++ porchfest/urls.py
@@ -10,7 +10,7 @@ admin.site.site_title			= 'Porchfest'
 urlpatterns = [
 	path('', include('website.urls')),
     path('obscure-map/', include('porchfestcore.urls')),
-    path('plan-your-day/', include('planyourday.urls')),
+    path('plan-your-day-now/', include('planyourday.urls')),
     path('dashboard/', include('porchpanel.urls')),
     path('porchfestpeeps/', admin.site.urls),
     path('api/porches/', include('porchfestcore.api.urls')),
