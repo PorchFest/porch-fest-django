ea0ff4da16d9b2f648059fdca8afdb9f278a98b6
Austin Reilly <funcychaos@gmail.com>
2026-03-23 22:22:28 -0700

    feat(planyourday): make performances scrolly

--- planyourday/views.py
+++ planyourday/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts           import render, get_object_or_404
-from datetime import datetime, date, timedelta
 from django.views.generic       import ListView
 from porchfestcore.models       import Performance
 from planyourday.api.filters    import PerformanceFilter
