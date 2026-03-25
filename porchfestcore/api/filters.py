d2b14c028638835c648a007a674f7025d65c3dc1
Austin Reilly <funcychaos@gmail.com>
2026-03-25 01:13:01 -0700

    feat(map): fix up map for genres model

--- porchfestcore/api/filters.py
+++ porchfestcore/api/filters.py
@@ -1,12 +1,12 @@
 import django_filters
-from porchfestcore.models   import Porch
+from porchfestcore.models   import Porch, Genre
 from django.db.models       import Q
 
 class PorchMapFilter(django_filters.FilterSet):
     search          = django_filters.CharFilter(method="filter_search")
-    genre           = django_filters.CharFilter(
-        field_name  ='performances__performer__genre',
-        lookup_expr ='iexact'
+    genres          = django_filters.MultipleChoiceFilter(
+        field_name='performances__performer__genres__slug',
+        choices=[],  # placeholder
     )
     after           = django_filters.TimeFilter(
         field_name  ='performances__start_time',
@@ -16,7 +16,12 @@ class PorchMapFilter(django_filters.FilterSet):
     sponsored       = django_filters.BooleanFilter(field_name='sponsored')
     class Meta:
         model       = Porch
-        fields      = ['genre', 'after', 'vendor', 'sponsored']
+        fields      = ['genres', 'after', 'vendor', 'sponsored']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filters['genres'].extra['choices'] = [
+            (g.slug, g.name) for g in Genre.objects.all()
+        ]
     def filter_search(self, queryset, name, value):
         return queryset.filter(
             Q(name__icontains=value) |
--- porchfestcore/views.py
+++ porchfestcore/views.py
@@ -2,10 +2,10 @@ from django.shortcuts 				import render
 from django.views.generic 			import TemplateView
 from django.http					import JsonResponse
 from django.views.decorators.csrf	import csrf_exempt
-from .models                        import Porch, Performer
+from .models                        import Porch, Performer, Genre
 
 def map_page(request):
-    genres = Performer.Genre.choices
+    genres = Genre.objects.all()
     return render(request, 'porchfestcore/map.html', {'genres': genres})
 
 
