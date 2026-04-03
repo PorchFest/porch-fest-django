811bf025ccd11776203ea1c9cb1bcb9a3f301e97
Austin Reilly <funcychaos@gmail.com>
2026-04-02 18:19:17 -0700

    feat(admin): make numbers editable

--- porchfestcore/admin.py
+++ porchfestcore/admin.py
@@ -83,6 +83,7 @@ class HasInvitationFilter(admin.SimpleListFilter):
 @admin.register(Porch)
 class PorchAdmin(admin.ModelAdmin):
     list_display			= ('name', 'number', 'owner_name', 'street_address', 'created_at',)
+    list_editable           = ('number',)
     search_fields 			= ('name', 'owner_name', 'owner_email', 'street_address', 'performances__performer__name')
     list_filter 			= ('approved', 'created_at', HasCoordinatesFilter, HasInvitationFilter, HasPerformancesFilter)
     formfield_overrides		= {
