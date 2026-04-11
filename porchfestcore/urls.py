af49c1df1adca1e712709bfc870edc96688e4ff4
Austin Reilly <funcychaos@gmail.com>
2026-04-10 19:57:49 -0700

    feat(map): auxillary map also oops

--- porchfestcore/urls.py
+++ porchfestcore/urls.py
@@ -4,4 +4,5 @@ from . 				import views
 urlpatterns = [
 	path('', views.coming_soon, name='map'),
 	path('obscure-map', views.map_page, name='obscure_map'),
+	path('marker-map', views.bland_map, name='marker_map'),
 ]
