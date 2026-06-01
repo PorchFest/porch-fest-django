3bd5e32eeab2a810fdca98e4520f42ba55b03945
Austin Reilly <funcychaos@gmail.com>
2026-06-01 08:55:39 -0700

    feat(rebuild): barely working front page, slowly rebuilding

--- src/apps/porchfest/models.py
+++ src/apps/porchfest/models.py
@@ -190,6 +190,20 @@ class Performance(models.Model):
         time_format = '%#I:%M %p' if os.name == 'nt' else '%-I:%M %p'
         return f"{self.performer} at {self.porch} ({self.start_time.strftime(time_format)})"
 
+class Itinerary(models.Model):
+    id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at      = models.DateTimeField(auto_now_add=True)
+    performances    = models.ManyToManyField('porchfestcore.Performance', related_name='itineraries', blank=True)
+
+    def ordered_performances(self):
+        return self.performances.select_related(
+            'performer',
+            'porch'
+        ).order_by('start_time')
+
+    def __str__(self):
+        return f"{self.id}"
+
 class TempUpload(models.Model):
     image 		= models.ImageField(upload_to='temp_uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
